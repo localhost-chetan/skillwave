335764afd153003eea6f60b42fef8309624d2e91
Chetan Seervi <chetanseervi513@gmail.com>
2025-10-25 17:54:25 +0530

    chore(db): update db.ts and README.md for drizzle studio setup

--- backend/src/lib/utils/db.ts
+++ backend/src/lib/utils/db.ts
@@ -1,5 +1,11 @@
 export const getDatabaseUrl = () => {
-    const DATABASE_URL = process.env.DATABASE_URL;
+    const isBunRuntime = typeof globalThis.Bun !== "undefined";
+    console.log(isBunRuntime ? "🚀 Running in Bun" : "🚀 Not running in Bun");
+
+    const DATABASE_URL = isBunRuntime ? process.env.DATABASE_URL : "postgresql://postgres:password@localhost:5432/skillwave";
+    
+    // If running postgres and the backend app in docker, use this instead:
+    // const DATABASE_URL = isBunRuntime ? process.env.DATABASE_URL : "postgresql://postgres:password@postgres:5432/skillwave";
 
     if (!DATABASE_URL) {
         throw new Error("DATABASE_URL is not set in environment variables");
